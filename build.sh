574a7dd7a579da6bec993c568d304d2cc0f39e73
HUY MINH <cnhuyminh1985@icloud.com>
2022-04-10 12:28:10 +0700

    updat

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 sourceList=( "NCDN" "NDEV" "NRESTAURANT" )
 
 cd "sources"
-cd "NSERVER"
-
-npm run build
-cd "dist/core/debug"
-zip -r "../../../../RELEASE/NSERVER.zip" *
-
-cd "../../../.."
 
 rm -rf "BUILD"
 rm -rf "RELEASE"
 
 mkdir "BUILD"
 mkdir "RELEASE"
+
+cd "NSERVER"
+npm run build
+cd "dist/core/debug"
+zip -r "../../../../RELEASE/NSERVER.zip" *
+
+cd "../../../.."
 
 cd "APPS"
 for item in "${sourceList[@]}"; do
